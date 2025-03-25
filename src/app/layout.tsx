1365653aa49e608d8285723418bd8377e77c26bd
Burhan <burhan@example.com>
2025-03-25 21:33:53 +0000

    Added Vercel Analytics and updated environment variables for deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/ThemeProvider'
 import { Toaster } from 'sonner'
 import React from 'react'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,12 +22,13 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className} suppressHydrationWarning>
-        <ClerkProvider publishableKey={process.env.CLERK_PUBLIC_KEY || process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
           <ThemeProvider>
             <div suppressHydrationWarning>
               {children}
             </div>
             <Toaster />
+            <Analytics />
           </ThemeProvider>
         </ClerkProvider>
       </body>
